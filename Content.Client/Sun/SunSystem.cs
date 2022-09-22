d6d3bf3ef3e7ac3fc4a358fc9dc9dee5e6ba74d7
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-09-22 20:26:56 +0800

    Sun

---
+++ Content.Client/Sun/SunSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Eye.Blinding;
+using Robust.Client.Graphics;
+
+namespace Content.Client.Sun;
+
+public sealed class SunSystem : EntitySystem
+{
+    [Dependency] ILightManager _lightManager = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BlindableComponent, ComponentInit>(OnSunInit);
+    }
+
+    private void OnSunInit(EntityUid uid, BlindableComponent component, ComponentInit args)
+    {
+        // The sun is bright. I don't like it. It's cool and all but I don't like how we have a massive ball of pure agonizing fire in the sky.
+        _lightManager.AmbientLightColor = Color.Gray;
+    }
+}
